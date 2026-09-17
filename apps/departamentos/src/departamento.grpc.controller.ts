3f1ae907fa2269314788ec8634612e10279961c8
Erik Neves <erikpdn1@gmail.com>
2026-09-17 10:08:38 -0300

    feat: cria controller grpc para receber requests do client

--- apps/departamentos/src/departamento.grpc.controller.ts
+++ apps/departamentos/src/departamento.grpc.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
+import { DepartamentoService } from './departamento.service.js';
+import {
+  DEPARTAMENTO_SERVICE_NAME,
+  GetDepartamentoRequest,
+} from '@app/contracts';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
-export class DepartamentoGrpcController {}
+export class DepartamentoGrpcController {
+  constructor(private readonly departamentoService: DepartamentoService) {}
+
+  @GrpcMethod(DEPARTAMENTO_SERVICE_NAME, 'GetDepartamento')
+  async getDepartamento({ id }: GetDepartamentoRequest) {
+    const departamento = await this.departamentoService.getDepartamento(id);
+  }
+}
--- apps/departamentos/src/departamento.service.ts
+++ apps/departamentos/src/departamento.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DepartamentoService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor() {}
+
+  async getDepartamento(id: string) {}
 }
